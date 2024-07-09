df9c9508183f8846517f59fe14faa0a34711329a
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2024-07-10 01:24:15 +0800

    Create customers model and migration

---
+++ database/migrations/2024_07_09_171220_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 13)->nullable(false);
+            $table->string('cellphone_number', 13);
+            $table->string('email', 75);
+            $table->string('messenger_name', 50);
+            $table->enum('status', ['0', '1'])->default('1')->nullable(false);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
