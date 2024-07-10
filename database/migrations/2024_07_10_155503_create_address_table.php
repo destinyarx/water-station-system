1b3fa65d3fbcf00df268a85ca4e1f868c9c3488d
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2024-07-11 00:04:36 +0800

    create model, controller and migration for address

---
+++ database/migrations/2024_07_10_155503_create_address_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('customer_id')->unsigned(); 
+            $table->foreign('customer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('description', 50)->nullable();
+            $table->string('unit_number', 35)->nullable();
+            $table->string('street', 35)->nullable();
+            $table->string('barangay', 35)->nullable();
+            $table->string('municipality', 35)->nullable();
+            $table->string('province', 35)->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('address');
+    }
+};
