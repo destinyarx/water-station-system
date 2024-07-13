dda908e99e1a63e655f4dbb57c7d540cbd7e06d0
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2024-07-14 00:37:07 +0800

    add model relationship

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -10,4 +10,8 @@ class Address extends Model
     use HasFactory;
 
     protected $table = 'address';
+
+    public function customer() {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
 }
