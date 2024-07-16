c6e631126e5cb1132860f1751c77b423f622b0c1
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2024-07-16 15:35:59 +0800

    init sales route and page

---
+++ app/Http/Controllers/SalesController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Sales;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SalesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Inertia::render('Sales', [
+            'page' => 'Sales Page',
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sales $sales)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sales $sales)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Sales $sales)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sales $sales)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -29,14 +30,15 @@ Route::middleware('auth')->group(function () {
 Route::get('/customer', [CustomerController::class, 'index'])->name('customer.index');
 
 
+// sales
+Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
+
+// Customer test
+Route::inertia('/customer/register', 'customer/Register');
+
+// test playground
 Route::inertia('/test', 'Test');
 Route::inertia('/prime', 'Prime');
-
-
-Route::inertia('/sales', 'Sales');
-
-// Customer
-Route::inertia('/customer/register', 'customer/Register');
 
 
 
