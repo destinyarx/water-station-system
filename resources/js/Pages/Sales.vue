3fffbcca48e571dea8dfb14af57e685a45180b05
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2024-07-16 15:35:40 +0800

    add -mcr for sales

--- resources/js/Pages/Sales.vue
+++ resources/js/Pages/Sales.vue
@@ -1,7 +1,23 @@
 <template>
 
+    <Header />
+
+    <div> {{ page }}</div>
 
 </template>
 
 <script setup>
+import { defineProps } from 'vue';
+import Header from '../Components/Layout/Header.vue'
+
+
+
+const props = defineProps({
+    page: {
+        type: String,
+        required: false
+    },
+})
+
+
 </script>
