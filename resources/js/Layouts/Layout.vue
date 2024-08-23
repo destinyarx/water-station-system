7d17b76b942bff75149f7d245098ba9275a7f229
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2024-08-23 21:44:49 +0800

    style sidebar

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="min-h-screen min-w-screen dark:bg-slate-400">
-        
         <Header />
 
         <div class="w-full flex flex-row">
             <div class="fixed top-0 left-0 z-50 h-full w-[5.1rem] group text-white bg-gray-800 transition-all duration-300 ease-in-out hover:w-[15.2rem]">
-                <div class="text-cyan-200 mt-5 mb-10">
-                    <i class="pi pi-amazon ml-6" style="font-size: 2.5rem"></i>
-                    <span class="text-xl text-zinc-50 opacity-0 group-hover:opacity-100 ml-2">Amazon</span>
+                <div class="w-full flex items-center mt-5 mb-10">
+                    <i class="pi pi-amazon text-cyan-300 ml-6" style="font-size: 2.5rem"></i>
+                    <span class="whitespace-nowrap inline-block overflow-hidden hidden group-hover:inline-block text-xl text-zinc-50 ml-2">Amazon</span>
                 </div>
                 <ul>
                     <li v-for="item in sidebarItems" class="my-10">
                         <Link :href="item.route">
+                          <div class="w-full flex items-center">
                             <i :class="item.icon" class="mr-1 ml-6 self-center" style="font-size: 1.5rem"></i>
-                            <span class="whitespace-nowrap ml-2 hidden group-hover:inline-block">
-                                {{ item.label }}
+                            <span class="whitespace-nowrap inline-block overflow-hidden hidden group-hover:inline-block ml-2">
+                              {{ item.label }}
                             </span>
+                          </div>
                         </Link>
                     </li>
                 </ul>
