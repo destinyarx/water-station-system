738517618aa49e96bbfd99c356667ad28e8e19d9
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2024-08-24 22:53:18 +0800

    add options config

---
+++ config/options.php
@@ -0,0 +1,25 @@
+<?php 
+return [
+
+    'frequency' => [
+        ['name' => 'Once', 'code' => 'O'],
+        ['name' => 'Daily', 'code' => 'D'],
+        ['name' => 'Every other Day', 'code' => 'EOD'],
+        ['name' => 'Once a week', 'code' => '1W'],
+        ['name' => 'Twice a week', 'code' => '2W'],
+        ['name' => 'Three times a week', 'code' => '3W'],
+        ['name' => 'Weekly (once a week)', 'code' => 'W1'],
+        ['name' => 'Every two weeks', 'code' => '2Wk'],
+    ],
+    
+    'scheduleGaps' => [
+        'D' => 1,      // Daily
+        'EOD' => 2,    // Every other Day
+        '1W' => 7,     // Once a week
+        '2W' => 3,     // Twice a week (assuming a 3-day gap)
+        '3W' => 2,     // Three times a week (assuming a 2-day gap)
+        'W1' => 7,     // Weekly (once a week)
+        '2Wk' => 14,   // Every two weeks
+    ],
+
+];
